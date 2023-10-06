02c0f0f1de0bd7f2eb04d889eee33e0bf3d989d8
silabanjames <silaban.james13@gmail.com>
2023-10-06 10:37:03 +0700

    add addUser page by james

--- src/pages/settings/add/addUser.vue
+++ src/pages/settings/add/addUser.vue
@@ -6,9 +6,9 @@
                     <h5>Add User </h5>
                 </div>
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-7 order-md-0 order-1">
-                            <form class="theme-form mega-form">
+                    <form class="theme-form mega-form">
+                        <div class="row">
+                            <div class="col-md-7 order-md-0 order-1 pe-5">
                                 <div class="mb-3">
                                     <label class="form-label">Name</label>
                                     <input class="form-control" type="text" placeholder="Name">
@@ -29,19 +29,16 @@
                                 <div class="mb-3 d-grid">
                                     <button class="btn btn-primary">Submit</button>
                                 </div>
-                            </form>
-                        </div>
-                        <div class="col-md-5 text-center mb-3">
-                            <div class="mb-3 border">
-                                <img class="img-fluid" src="@/assets/images/ecommerce/card.png" alt="">
-                                <input class="form-control w-50 d-flex justify-content-center" type="file">
                             </div>
-                            <!-- <div class="mb-5 row">
-                                <div>
+                        
+                            <div class="col-md-5 text-center">
+                                <div class="d-flex flex-column align-items-center">
+                                    <img class="img-fluid mb-2" src="@/assets/images/ecommerce/card.png" alt="">
+                                    <input class="form-control w-100" type="file">
                                 </div>
-                            </div> -->
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
