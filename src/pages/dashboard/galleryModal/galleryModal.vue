d7bac0158377fbaa976f4a5a600a4c81f7df6a37
silabanjames <silaban.james13@gmail.com>
2023-10-11 11:44:17 +0700

    add tag

--- src/pages/dashboard/galleryModal/galleryModal.vue
+++ src/pages/dashboard/galleryModal/galleryModal.vue
@@ -15,16 +15,40 @@
 
             </div>
           </div>
-          <div class="col-3 justify-content-between">
+          <div class="col-lg-3 justify-content-between">
             <div>
-              <div class="example" id="example"></div>
+              <div class="colorPicker" id="colorPicker" style="width: 100%;"></div>
+              <!-- <v-color-picker></v-color-picker> -->
+              <!-- <input type="color"> -->
+            </div>
+
+            <!-- Tag -->
+            <div class="tag my-1">
+              <div class="tag-header p-1">
+                Tag
+              </div>
+              <div class="p-1">
+                <div class="d-flex justify-content-between bg-success p-2 rounded-2 my-1 ">
+                  <span class="fw-semibold text-black">Productive</span>
+                  <span class="bg-white px-2 rounded-pill text-black">7</span>
+                </div>
+                <div class="d-flex justify-content-between bg-warning p-2 rounded-2 my-1 ">
+                  <span class="fw-semibold text-black">Semi Productive</span>
+                  <span  class="bg-white px-2 rounded-pill text-black">7</span>
+                </div>
+                <div class="d-flex justify-content-between bg-danger p-2 rounded-2 my-1 ">
+                  <span class="fw-semibold text-white">Non Productive</span>
+                  <span  class="bg-white px-2 rounded-pill text-black">7</span>
+                </div>
+              </div>
             </div>
 
             <!-- Modal Button -->
-            <div class="d-flex justify-content-between">
+            <div class="d-flex flex-wrap justify-content-between">
               <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
               <button class="btn btn-primary" type="button">Save changes</button>
             </div>
+
           </div>
         </div>
 
@@ -32,20 +56,54 @@
     </div>
 </template>
 
+<style scoped>
+.layout_picker{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: stretch;
+  font-size: 10px;
+  width: 100%;
+  padding: 0.5em;
+}
+
+.tag{
+  border-style: solid;
+  border-width: 3px;
+  border-radius: 5px;
+}
+
+.tag-header{
+  border-bottom: 3px solid black;
+  margin: 0 0;
+}
+</style>
+
 <script>
 import Picker from 'vanilla-picker';
 
 export default{
+  data(){
+    return {
+      color: ''
+    }
+  },
   mounted(){
-    console.log('run')
     new Picker({
-        parent: document.querySelector('#example'),
-        popup: false,
-        alpha: false,
-        layout: 'default',
-        editor: true,
-        editorFormat: 'hex',
-      });
+      parent: document.querySelector('#colorPicker'),
+      popup: false,
+      alpha: false,
+      layout: 'default',
+      editor: true,
+      editorFormat: 'hex',
+      onChange: (color) => {
+        this.color = color.hex
+        this.color = this.color.slice(0,7)
+      }
+    });
+
+    
   }
 }
 </script>
