a2de00344d440bd3fc051f42ce2ee7a5f1c280f9
gatotbima1104 <gatotbima2002@gmail.com>
2023-10-13 15:06:25 +0700

    add: alarm-function

--- src/pages/settings/userManagement/setAlarm.vue
+++ src/pages/settings/userManagement/setAlarm.vue
@@ -10,14 +10,52 @@
                         <label for="" class="col-form-label">Time (s)</label>
                     </div>
                     <div class="mb-3 col p-r-0">
-                        <input class="form-control" type="number" placeholder="Set Alarm">
+                        <input class="form-control" type="number" v-model="sound.alarmTimeout" placeholder="Set Alarm" />
                     </div>
                     <div class="col-12 d-flex justify-content-end">
-                        <button class="btn btn-primary btn-block" type="button" title="">Submit</button>
+                        <button class="btn btn-block" :class="{'btn-danger': sound.isAlarmActive, 'btn-primary': !sound.isAlarmActive,}" type="button" @click="sound.isAlarmActive ? stopAlarm() : setAlarm()">
+                        {{ sound.isAlarmActive ? 'Stop' : 'Submit' }}
+                        </button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-    </template>
-    
+</template>
+
+<script setup>
+import { Howl } from "howler";
+import alarmSound from "../../../../src/assets/audio/mixkit-city-alert-siren-loop-1008.wav";
+import { reactive } from "vue";
+
+const sound = reactive({
+    alarmTimeout: 0,
+    isAlarmActive: false,
+});
+
+// SET ALARM WITH HOWLER
+const alarm = new Howl({
+    src: [alarmSound],
+    loop: true,
+    volume: 0.5,
+});
+
+const setAlarm = () => {
+    const timeout = sound.alarmTimeout * 1000;
+    console.log("Setting Alarm for " + timeout + " Seconds");
+
+    setTimeout(() => {
+        console.log("Violance detected");
+        alarm.play();
+
+        sound.isAlarmActive = true;
+        sound.alarmTimeout = 0;
+    }, timeout);
+};
+
+const stopAlarm = () => {
+    console.log("Alarm Stopped");
+    alarm.stop();
+    sound.isAlarmActive = false;
+};
+</script>
