ec2334cc2c5c61a837078bd04e66d8e0f0556c5a
silabanjames <silaban.james13@gmail.com>
2023-10-26 14:23:27 +0700

    add axios to log page

--- src/store/modules/log.js
+++ src/store/modules/log.js
@@ -1,3 +1,7 @@
+import axiosInstance from '../../lib/index'
+import router from '../../router/index'
+
+// Data dummy
 import trackingList from "../../data/log/trackingList.json"
 
 const state = {
@@ -32,12 +36,29 @@ const mutations = {
     deleteTrack: (state, payload) => {
         state.trackingList = state.trackingList.filter(data => data.id !== payload);
     },
-    
+    getRequestData(state, payload){
+        state.trackingList = payload
+    }
 };
+
+const actions={
+    async getRequestData(context){
+        /*
+        * Edit 'track_information' untuk mendapatkan data
+        */
+        await axiosInstance.get('/auth/sign-in')
+        .then(res => res.data)
+        .then(data => {
+            context.commit('getRequestData', data.track_information)
+        })
+        .catch( err => console.log(err) )
+    }
+}
 
 export default {
     namespaced: true,
     state,
     mutations,
-    getters
+    getters,
+    actions
 };
